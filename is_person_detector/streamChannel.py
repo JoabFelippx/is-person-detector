3a50d683523e3812044630e78d97926ba43ff9f6
JoabFelippx <80433162+JoabFelippx@users.noreply.github.com>
2024-10-11 09:42:49 -0400

    Update streamChannel.py

--- is_person_detector/streamChannel.py
+++ is_person_detector/streamChannel.py
@@ -3,7 +3,7 @@ import socket
 from is_wire.core import Channel
 
 class StreamChannel(Channel):
-    def __init__(self, uri="amqp://guest:guest@localhost:5672", exchange="is"):
+    def __init__(self, uri="amqp://guest:guest@<ip>:<port>", exchange="is"):
         super().__init__(uri=uri, exchange=exchange)
 
     def consume_last(self, return_dropped=False):
@@ -12,7 +12,7 @@ class StreamChannel(Channel):
             msg = super().consume(timeout=0.1)
         except socket.timeout:
             return False
-            
+
         while True:
             try:
                 # will raise an exceptin when no message remained
@@ -20,3 +20,4 @@ class StreamChannel(Channel):
                 dropped += 1
             except socket.timeout:
                 return (msg, dropped) if return_dropped else msg
+
