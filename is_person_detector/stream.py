6ea40afae8ee04ff6f47aff99aaa86226058caaf
Joab Felippe <80433162+JoabFelippx@users.noreply.github.com>
2025-03-13 09:34:17 -0300

    Update stream.py

--- is_person_detector/stream.py
+++ is_person_detector/stream.py
@@ -1,3 +1,4 @@
+
 from is_msgs.image_pb2 import Image
 from is_wire.core import Logger, Subscription, Message, Tracer, AsyncTransport
 from opencensus.trace.span import Span
@@ -11,8 +12,8 @@ import re
 
 def main() -> None:
     
-    broker_uri = 'amqp://guest:guest@<ip>:<port>'
-    zipkin_host = 'http://<ip:<port>/'
+    broker_uri = 'amqp://guest:guest@10.10.2.211:30000'
+    zipkin_host = 'http://10.10.2.211:30200/'
     
     service_name = 'Person.Detector'
     
@@ -28,23 +29,23 @@ def main() -> None:
     subscription.subscribe('CameraGateway.*.Frame')
     
     while True:
-
+        
         msg = channel.consume_last()
 
         tracer = Tracer(exporter=exporter, span_context=msg.extract_tracing())
         span = tracer.start_span(name='detection_and_render')
-
+        
         detection_span = None
-
+        
         with tracer.span(name='unpack'):
             img = msg.unpack(Image)
             im_np = to_np(img)
-
+            
         with tracer.span(name='detection') as _span:
             camera_id = get_topic_id(msg.topic)
             detections = person_detector.detect(im_np)
             detection_span = _span
-
+            
         with tracer.span(name='pack_and_publish_detections'):
             person_msg = Message()
             person_msg.topic = f'PersonDetector.{camera_id}.Detection'
@@ -52,23 +53,29 @@ def main() -> None:
 
             bounding_boxes = detections[0].boxes.xyxy
             obj_annotations = person_detector.to_object_annotations(bounding_boxes, detections[0].orig_shape) 
-            image_with_bounding = person_detector.bounding_box(im_np, obj_annotations)
-
-            person_msg.pack(to_image(image_with_bounding))
             channel.publish(person_msg)
 
+        with tracer.span(name='render_pack_publish'):
+
+            image_with_bounding = person_detector.bounding_box(im_np, obj_annotations)
+            rendered_msg = Message()
+            rendered_msg.topic = f'PersonDetector.{camera_id}.Rendered'
+            rendered_msg.inject_tracing(span)
+
+            rendered_msg.pack(to_image(image_with_bounding))
+            channel.publish(rendered_msg)
+        
         span.add_attribute('Detections', len(detections[0].boxes))
         tracer.end_span()
-
+        
         info = {
             'detections': len(detections[0].boxes),
-           #'dropped_messages': dropped,
-            'took_ms': {
+               'took_ms': {
                 'detection': round(span_duration_ms(detection_span), 2),
                 'service': round(span_duration_ms(span), 2),
             },   
         }
-        log.info('{}', str(info).replace("'", '"'))         
-
+        log.info('{}', str(info).replace("'", '"'))        
+    
 if __name__ == '__main__':
     main()
