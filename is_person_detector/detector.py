fca15c8acd2b8e25bd0ff2e58281b677127aabca
Joab Felippe <80433162+JoabFelippx@users.noreply.github.com>
2025-03-13 09:32:29 -0300

    Update detector.py

--- is_person_detector/detector.py
+++ is_person_detector/detector.py
@@ -1,8 +1,4 @@
 from ultralytics import YOLO
-
-# from typing import Tuple
-# from nptyping import NDArray, Int8, Float32, Shape
-
 import cv2
 import numpy as np
 
@@ -14,22 +10,24 @@ Channels = int
 
 class personDetector:
     def __init__(self):
-        self.model = YOLO('./model/yolov8s.pt')
-        self.model.to('cuda')
+        self.model = YOLO('../model/yolov8m-pose.pt')
+        self.model.to('cuda')        
     @staticmethod
     def bounding_box(image, annotations: ObjectAnnotations):
+
+        image_2 = image
         for obj in annotations.objects:
             x1 = int(obj.region.vertices[0].x)
             y1 = int(obj.region.vertices[0].y)
             x2 = int(obj.region.vertices[1].x)
             y2 = int(obj.region.vertices[1].y)
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        return image
-    
+            cv2.rectangle(image_2, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        return image_2
+  
     @staticmethod
-    def to_object_annotations(results, image_shape,) -> ObjectAnnotations:
+    def to_object_annotations(bounding_coords, image_shape,) -> ObjectAnnotations:
             annotations = ObjectAnnotations()
-            for det in results:
+            for det in bounding_coords:
                 bounding_box = det[0:4].cpu().numpy().astype(np.int32)
                 item = annotations.objects.add()
                 vertex_1 = item.region.vertices.add()
@@ -37,7 +35,7 @@ class personDetector:
                 vertex_1.y = bounding_box[1]
                 vertex_2 = item.region.vertices.add()
                 vertex_2.x = bounding_box[2]
-                vertex_2.y = bounding_box[3]
+                vertex_2.y = bounding_box[3]            
                 item.label = "person"
                 item.score = det[-1]
             annotations.resolution.width = image_shape[1]
@@ -46,5 +44,5 @@ class personDetector:
         
     def detect(self, array) -> ObjectAnnotations:
         
-        results = self.model(array, classes=[0])
+        results = self.model(array)
         return results
