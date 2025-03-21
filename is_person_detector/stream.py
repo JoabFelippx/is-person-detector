f2b0335a66364cada2e34a5c8204f5225e577f3e
Joab Felippe <80433162+JoabFelippx@users.noreply.github.com>
2025-03-21 17:10:13 -0300

    Update stream.py

--- is_person_detector/stream.py
+++ is_person_detector/stream.py
@@ -1,4 +1,3 @@
-
 from is_msgs.image_pb2 import Image
 from is_wire.core import Logger, Subscription, Message, Tracer, AsyncTransport
 from opencensus.trace.span import Span
@@ -9,43 +8,72 @@ from streamChannel import StreamChannel
 from utils import get_topic_id, to_np, to_image, create_exporter, span_duration_ms
 
 import re
+import time
+import socket
+import json
 
 def main() -> None:
-    
-    broker_uri = 'amqp://guest:guest@10.10.2.211:30000'
-    zipkin_host = 'http://10.10.2.211:30200/'
-    
+
+    config = json.load(open('../etc/conf/options.json'))
+    broker_uri = config['broker_uri']
+    zipkin_host = config['zipkin_uri']
+
     service_name = 'Person.Detector'
-    
+
     person_detector = personDetector()
-    
+
     log = Logger(name=service_name)
     channel = StreamChannel()
     log.info(f'Connected to broker {broker_uri}')
-    
+
     exporter = create_exporter(service_name, zipkin_host, log)
 
     subscription = Subscription(channel=channel, name=service_name)
     subscription.subscribe('CameraGateway.*.Frame')
-    
+
     while True:
-        
+
         msg = channel.consume_last()
+
+        if msg.topic != "CameraGateway.5.Frame": #Camera 5 doesn't have "inject_tracing"
+
+            timestamp_rcvd = time.time()
+
+            msg_contrace = (
+                f'{{"timestamp_send": "{int(msg.created_at*1000000)}", '
+                f'"timestamp_rcvd": "{int(timestamp_rcvd*1000000)}", '
+                f'"x-b3-flags": "{msg.metadata["x-b3-flags"]}", '
+                f'"x-b3-parentspanid": "{msg.metadata["x-b3-parentspanid"]}", '
+                f'"x-b3-sampled": "{msg.metadata["x-b3-sampled"]}", '
+                f'"x-b3-spanid": "{msg.metadata["x-b3-spanid"]}", '
+                f'"x-b3-traceid": "{msg.metadata["x-b3-traceid"]}", '
+                f'"spanname": "frame"}}'
+            )
+            print(msg_contrace)
+            bytesToSend = str.encode(msg_contrace)
+
+            serverAddressPort = (config['conmtrace_host'], config['conmtrace_port'])
+            bufferSize = 2048
+
+            UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+            log.info("Enviando mensagem para Contrace")
+            UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+            log.info("Mensagem para Contrace: {}", msg_contrace)
 
         tracer = Tracer(exporter=exporter, span_context=msg.extract_tracing())
         span = tracer.start_span(name='detection_and_render')
-        
+
         detection_span = None
-        
+
         with tracer.span(name='unpack'):
             img = msg.unpack(Image)
             im_np = to_np(img)
-            
+
         with tracer.span(name='detection') as _span:
             camera_id = get_topic_id(msg.topic)
             detections = person_detector.detect(im_np)
             detection_span = _span
-            
+
         with tracer.span(name='pack_and_publish_detections'):
             person_msg = Message()
             person_msg.topic = f'PersonDetector.{camera_id}.Detection'
@@ -63,19 +91,21 @@ def main() -> None:
             rendered_msg.inject_tracing(span)
 
             rendered_msg.pack(to_image(image_with_bounding))
+            rendered_msg.created_at = time.time()
+            print(rendered_msg.created_at)
             channel.publish(rendered_msg)
-        
+
         span.add_attribute('Detections', len(detections[0].boxes))
         tracer.end_span()
-        
+
         info = {
             'detections': len(detections[0].boxes),
                'took_ms': {
                 'detection': round(span_duration_ms(detection_span), 2),
                 'service': round(span_duration_ms(span), 2),
-            },   
+            },
         }
-        log.info('{}', str(info).replace("'", '"'))        
-    
+        log.info('{}', str(info).replace("'", '"'))
+
 if __name__ == '__main__':
     main()
