fc80f232987d0d47cdeaa82bbe7b8536867c4092
Joab Felippe <80433162+JoabFelippx@users.noreply.github.com>
2025-03-26 12:52:08 -0300

    Update stream.py

--- is_person_detector/stream.py
+++ is_person_detector/stream.py
@@ -5,12 +5,24 @@ from opencensus.ext.zipkin.trace_exporter import ZipkinExporter
 from detector import personDetector
 
 from streamChannel import StreamChannel
-from utils import get_topic_id, to_np, to_image, create_exporter, span_duration_ms
+from utils import get_topic_id, to_np, to_image, create_exporter, span_duration_ms, msg_commtrace
 
 import re
 import time
 import socket
 import json
+
+def send_commtrace_msg(msg:str,timestamp_rcvd:int,serverAddressPort:str,log:Logger,bufferSize=2048):
+    if msg.metadata != {}:
+        bytesToSend, msg_to_commtrace = msg_commtrace(msg,timestamp_rcvd)
+
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        log.info("Enviando mensagem para Contrace")
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        log.info("Mensagem para Contrace: {}", msg_to_commtrace)
+    else:
+        log.warn("Could not send message to Commtrace because msg.metadata is empty")
+
 
 def main() -> None:
 
@@ -35,30 +47,9 @@ def main() -> None:
 
         msg = channel.consume_last()
 
-        if msg.topic != "CameraGateway.5.Frame": #Camera 5 doesn't have "inject_tracing"
-
-            timestamp_rcvd = time.time()
-
-            msg_contrace = (
-                f'{{"timestamp_send": "{int(msg.created_at*1000000)}", '
-                f'"timestamp_rcvd": "{int(timestamp_rcvd*1000000)}", '
-                f'"x-b3-flags": "{msg.metadata["x-b3-flags"]}", '
-                f'"x-b3-parentspanid": "{msg.metadata["x-b3-parentspanid"]}", '
-                f'"x-b3-sampled": "{msg.metadata["x-b3-sampled"]}", '
-                f'"x-b3-spanid": "{msg.metadata["x-b3-spanid"]}", '
-                f'"x-b3-traceid": "{msg.metadata["x-b3-traceid"]}", '
-                f'"spanname": "frame"}}'
-            )
-            print(msg_contrace)
-            bytesToSend = str.encode(msg_contrace)
-
-            serverAddressPort = (config['conmtrace_host'], config['conmtrace_port'])
-            bufferSize = 2048
-
-            UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-            log.info("Enviando mensagem para Contrace")
-            UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-            log.info("Mensagem para Contrace: {}", msg_contrace)
+        timestamp_rcvd = time.time()
+        serverAddressPort = (config['conmtrace_host'], config['conmtrace_port'])
+        send_commtrace_msg(msg,timestamp_rcvd,serverAddressPort,log)
 
         tracer = Tracer(exporter=exporter, span_context=msg.extract_tracing())
         span = tracer.start_span(name='detection_and_render')
@@ -80,7 +71,8 @@ def main() -> None:
             person_msg.inject_tracing(span)
 
             bounding_boxes = detections[0].boxes.xyxy
-            obj_annotations = person_detector.to_object_annotations(bounding_boxes, detections[0].orig_shape) 
+            obj_annotations = person_detector.to_object_annotations(bounding_boxes, detections[0].orig_shape)
+            person_msg.created_at = time.time()
             channel.publish(person_msg)
 
         with tracer.span(name='render_pack_publish'):
@@ -108,3 +100,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
